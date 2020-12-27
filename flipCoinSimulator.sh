07858f9a6929e35806cdf41a522d82993d2caf0a
joel3216 <joeljaxon3216@gmail.com>
2020-12-27 16:36:14 +0530

    ADD. created a for loop to repeat the coin flip and count heads and tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,14 +4,20 @@
 
 heads=1
 tails=0
+headTally=0
+tailTally=0
 
-flipCoin=$((RANDOM%2))
+for ((i=0; i<10; i++))
+do
+	flipCoin=$((RANDOM%2))
+	if [[ $flipCoin -eq $heads ]]
+	then
+		headTally=$(($headTally+1))
+	else
+		tailTally=$(($tailTally+1))
+	fi
+done
 
-if [[ $flipCoin -eq $heads ]]
-then
-	winner="heads"
-else
-	winner="Tails"
-fi
+echo "number of times heads won : "$headTally
+echo "number of times tails won : "$tailTally
 
-echo "winner is "$winner
