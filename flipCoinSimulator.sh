0638cb09b020c2bb66e8c83566e21f23d040aff9
joel3216 <joeljaxon3216@gmail.com>
2020-12-27 14:32:39 +0530

    ADD. added code to display the winner of coin flip

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
 
 #flip coin simulation assignment
+
+heads=1
+tails=0
+
+flipCoin=$((RANDOM%2))
+
+if [[ $flipCoin -eq $heads ]]
+then
+	winner="heads"
+else
+	winner="Tails"
+fi
+
+echo "winner is "$winner
